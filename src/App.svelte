5e075e40ca2e36b53461b8dab3cda9696369e2ec
ratsounds <ratsounds@gmail.com>
2020-10-30 11:20:24 +0000

    fix ogp

--- src/App.svelte
+++ src/App.svelte
@@ -4,25 +4,30 @@
 </script>
 
 <style>
-  main {
-    width: 100%;
-    height:100vh;
-    overflow: hidden;
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    grid-template-columns: 1fr;
-    align-items: center;
-    justify-content: center;
-  }
+    main {
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        grid-template-columns: 1fr;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
 
 <svelte:head>
-  <meta property="og:title" content="SAO:MD:VA">
-  <meta property="og:site_name" content="SAO:MD:VA">
-  <meta property="og:url" content="https://ratsounds.github.io/saomdva/">
-  <meta property="og:description" content="This web app is a tool for SAO:MD(Sword Art Online - Memory Defrag) skill durations video analysis.">
-  <meta property="og:type" content="article">
-  <meta property="og:image" content="https://ratsounds.github.io/saomdva/ogp.png">    
+    <meta property="og:title" content="SAO:MD:VA" />
+    <meta property="og:site_name" content="SAO:MD:VA" />
+    <meta property="og:url" content="https://ratsounds.github.io/saomdva/" />
+    <meta
+        property="og:description"
+        content="This web app is a tool for SAO:MD(Sword Art Online - Memory Defrag) skill durations video analysis." />
+    <meta property="og:type" content="article" />
+    <meta property="og:image" content="https://ratsounds.github.io/saomdva/ogp.png" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://ratsounds.github.io/saomdva/" />
+    <meta property="twitter:site" content="@ratsounds" />
 </svelte:head>
 <main>
     <VA />
